827a941054f99699653a0ee643f1ad51ba9f6074
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-11 11:33:26 +0300

    basic python syntax

---
+++ python/python_types.py
@@ -0,0 +1,37 @@
+#Data types
+
+# 1. Integers
+a = 5
+b = -10
+print(type(a))  # Output: <class 'int'>
+print(type(b))  # Output: <class 'int'>
+print(a + b)  # Output: -5
+
+ 
+# 2. Floats
+c = 3.14
+d = -2.5
+print(type(c))  # Output: <class 'float'>
+print(type(d))  # Output: <class 'float'>
+print(c + d)  # Output: 0.6399999999999999
+
+# 3. Strings
+e = "Hello"
+f = 'World' 
+print(type(e))  # Output: <class 'str'>
+print(type(f))  # Output: <class 'str'>
+print(e + " " + f)  # Output: Hello World
+
+# 4. Lists
+g = [1, 2, 3]
+h = ["apple", "banana", "cherry"]
+print(type(g))  # Output: <class 'list'>
+print(type(h))  # Output: <class 'list'>
+print(g + h)  # Output: [1, 2, 3, 'apple', 'banana', 'cherry']
+
+# 5. Tuples
+i = (1, 2, 3)      
+j = ("apple", "banana", "cherry")
+print(type(i))  # Output: <class 'tuple'>
+print(type(j))  # Output: <class 'tuple'>
+print(i + j)  # Output: (1, 2, 3, 'apple', 'banana', 'cherry')    
---
+++ python/whileloop.py
@@ -0,0 +1,12 @@
+def get_values():
+    print("start")
+
+flag = True
+while flag:
+        print("Loop")
+        flag = False
+
+
+
+
+get_values()
