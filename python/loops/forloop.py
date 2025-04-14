7ea8f978abdf92bdac8513a4996e91e4436d0111
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-14 20:53:59 +0300

    sets and dictionaries

--- python/loops/forloop.py
+++ python/loops/forloop.py
@@ -19,3 +19,20 @@ def get_data():
         print(i)
 get_data()
 #using set
+def get_data():
+    my_set = {1, 2, 3, 4, 5}
+    for i in my_set:
+        print(i)
+get_data()
+#using dictionary
+def get_data():
+    my_dict = {'a': 1, 'b': 2, 'c': 3}
+    for key in my_dict:
+        print(key, my_dict[key])
+get_data()
+#using string
+def get_data():
+    my_string = "Hello"
+    for i in my_string:
+        print(i)
+get_data()
