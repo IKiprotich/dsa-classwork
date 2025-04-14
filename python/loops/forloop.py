5f24e31a15bfbf67ebf9ceb6d9e07c233dd58db4
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-14 20:52:50 +0300

    lists and tuples

--- python/loops/forloop.py
+++ python/loops/forloop.py
@@ -7,3 +7,15 @@ def get_data():
 get_data()
 
 #using list
+def get_data():
+    my_list = [1, 2, 3, 4, 5]
+    for i in my_list:
+        print(i)
+get_data()
+#using tuple    
+def get_data():
+    my_tuple = (1, 2, 3, 4, 5)
+    for i in my_tuple:
+        print(i)
+get_data()
+#using set
