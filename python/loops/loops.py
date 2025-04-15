93f2b636e3b8a88db302b8d7e9fde60a61c7b4db
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-15 09:15:25 +0300

    loops

---
+++ python/loops/loops.py
@@ -0,0 +1,13 @@
+#while loop
+nt = 0
+while nt < 5:
+    print("Hello")
+    nt += 1 #or nt = nt + 1
+# for loop
+names = ["John", "Jane", "Doe"]
+for name in names:
+    print(name)
+
+#loops with range
+for i in range(5):
+    print(i)
