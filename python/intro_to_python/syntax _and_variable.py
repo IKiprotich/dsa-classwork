77b90d0b828b99b606c353f3b9d507a0be4b0b6e
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-15 09:02:38 +0300

    loops and syntax

---
+++ python/intro_to_python/syntax _and_variable.py
@@ -0,0 +1,8 @@
+user_name = "John"
+flag = False
+id = 12345
+height = 5.9
+
+
+to_float = int(height)
+print(to_float)
---
+++ python/loops/if_else.py
@@ -0,0 +1,16 @@
+a = 12
+b = 10 
+if a > b:
+    print("a is greater than b")
+    
+if (a > b):
+    print("a is greater than b")
+else:
+    print("a is less than b")
+
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
